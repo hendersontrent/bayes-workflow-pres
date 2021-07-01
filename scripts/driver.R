159922e42de42c9b5334bdb1fba30c3cbe963f37
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-07-01 17:39:11 +1000

    More Bayesian work

--- scripts/driver.R
+++ scripts/driver.R
@@ -16,6 +16,7 @@ library(tidyr)
 library(ggplot2)
 library(scales)
 library(tibble)
+library(janitor)
 library(foreign)
 library(Rcatch22)
 library(theft)
@@ -306,46 +307,72 @@ testNormed <- test %>%
 
 trainWide <- trainNormed %>%
   pivot_wider(id_cols = c(id, group), names_from = "names", values_from = "values") %>%
-  drop_na()
+  drop_na() %>%
+  mutate(group = ifelse(group == "1", 0, 1))
 
 testWide <- testNormed %>%
   pivot_wider(id_cols = c(id, group), names_from = "names", values_from = "values") %>%
-  drop_na()
+  drop_na() %>%
+  mutate(group = ifelse(group == "1", 0, 1))
 
 #-------------------------------------------------------------------------------
 #--------------------------FIT UNINFORMED MODEL 1------------------------------
 #-------------------------------------------------------------------------------
 
+# Plot priors
+
+data.frame(x = seq(from = -5, to = 5, by = 0.1)) %>%
+  mutate(y = LaplacesDemon::dst(x, mu = 0, sigma = 2.5, nu = 7),
+         category = "Prior") %>%
+  ggplot(aes(x = x, y = y, colour = category)) +
+  geom_line(size = 1.25) +
+  labs(title = "Our vague coefficient prior",
+       x = "x",
+       y = "Probability Density") +
+  scale_colour_manual(values = palette) +
+  theme(legend.position = "none")
+
+data.frame(x = seq(from = -5, to = 5, by = 0.1)) %>%
+  mutate(y = dnorm(x, mean = 0, sd = 1),
+         category = "Prior") %>%
+  ggplot(aes(x = x, y = y, colour = category)) +
+  geom_line(size = 1.25) +
+  labs(title = "Our vague intercept prior",
+       x = "x",
+       y = "Probability Density") +
+  scale_colour_manual(values = palette) +
+  theme(legend.position = "none")
+
 options(mc.cores = parallel::detectCores())
 
 # Build model with vague and uninformative priors
 
-stan_data <- list(N = nrow(feature_matrix3),
-                  y = feature_matrix3$group,
-                  DN_HistogramMode_5 = feature_matrix3$DN_HistogramMode_5,
-                  DN_HistogramMode_10 = feature_matrix3$DN_HistogramMode_10,
-                  CO_f1ecac = feature_matrix3$CO_f1ecac,
-                  CO_FirstMin_ac = feature_matrix3$CO_FirstMin_ac,
-                  CO_HistogramAMI_even_2_5 = feature_matrix3$CO_HistogramAMI_even_2_5,
-                  CO_trev_1_num = feature_matrix3$CO_trev_1_num,
-                  MD_hrv_classic_pnn40 = feature_matrix3$MD_hrv_classic_pnn40,
-                  SB_BinaryStats_mean_longstretch1 = feature_matrix3$SB_BinaryStats_mean_longstretch1,
-                  SB_TransitionMatrix_3ac_sumdiagcov = feature_matrix3$SB_TransitionMatrix_3ac_sumdiagcov,
-                  PD_PeriodicityWang_th0_01 = feature_matrix3$PD_PeriodicityWang_th0_01,
-                  CO_Embed2_Dist_tau_d_expfit_meandiff = feature_matrix3$CO_Embed2_Dist_tau_d_expfit_meandiff,
-                  IN_AutoMutualInfoStats_40_gaussian_fmmi = feature_matrix3$IN_AutoMutualInfoStats_40_gaussian_fmmi,
-                  FC_LocalSimple_mean1_tauresrat = feature_matrix3$FC_LocalSimple_mean1_tauresrat,
-                  DN_OutlierInclude_p_001_mdrmd = feature_matrix3$DN_OutlierInclude_p_001_mdrmd,
-                  DN_OutlierInclude_n_001_mdrmd = feature_matrix3$DN_OutlierInclude_n_001_mdrmd,
-                  SP_Summaries_welch_rect_area_5_1 = feature_matrix3$SP_Summaries_welch_rect_area_5_1,
-                  SB_BinaryStats_diff_longstretch0 = feature_matrix3$SB_BinaryStats_diff_longstretch0,
-                  SB_MotifThree_quantile_hh = feature_matrix3$SB_MotifThree_quantile_hh,
-                  SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1 = feature_matrix3$SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1,
-                  SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1 = feature_matrix3$SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1,
-                  SP_Summaries_welch_rect_centroid = feature_matrix3$SP_Summaries_welch_rect_centroid,
-                  FC_LocalSimple_mean3_stderr = feature_matrix3$FC_LocalSimple_mean3_stderr)
+stan_data <- list(N = nrow(trainWide),
+                  y = trainWide$group,
+                  DN_HistogramMode_5 = trainWide$DN_HistogramMode_5,
+                  DN_HistogramMode_10 = trainWide$DN_HistogramMode_10,
+                  CO_f1ecac = trainWide$CO_f1ecac,
+                  CO_FirstMin_ac = trainWide$CO_FirstMin_ac,
+                  CO_HistogramAMI_even_2_5 = trainWide$CO_HistogramAMI_even_2_5,
+                  CO_trev_1_num = trainWide$CO_trev_1_num,
+                  MD_hrv_classic_pnn40 = trainWide$MD_hrv_classic_pnn40,
+                  SB_BinaryStats_mean_longstretch1 = trainWide$SB_BinaryStats_mean_longstretch1,
+                  SB_TransitionMatrix_3ac_sumdiagcov = trainWide$SB_TransitionMatrix_3ac_sumdiagcov,
+                  PD_PeriodicityWang_th0_01 = trainWide$PD_PeriodicityWang_th0_01,
+                  CO_Embed2_Dist_tau_d_expfit_meandiff = trainWide$CO_Embed2_Dist_tau_d_expfit_meandiff,
+                  IN_AutoMutualInfoStats_40_gaussian_fmmi = trainWide$IN_AutoMutualInfoStats_40_gaussian_fmmi,
+                  FC_LocalSimple_mean1_tauresrat = trainWide$FC_LocalSimple_mean1_tauresrat,
+                  DN_OutlierInclude_p_001_mdrmd = trainWide$DN_OutlierInclude_p_001_mdrmd,
+                  DN_OutlierInclude_n_001_mdrmd = trainWide$DN_OutlierInclude_n_001_mdrmd,
+                  SP_Summaries_welch_rect_area_5_1 = trainWide$SP_Summaries_welch_rect_area_5_1,
+                  SB_BinaryStats_diff_longstretch0 = trainWide$SB_BinaryStats_diff_longstretch0,
+                  SB_MotifThree_quantile_hh = trainWide$SB_MotifThree_quantile_hh,
+                  SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1 = trainWide$SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1,
+                  SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1 = trainWide$SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1,
+                  SP_Summaries_welch_rect_centroid = trainWide$SP_Summaries_welch_rect_centroid,
+                  FC_LocalSimple_mean3_stderr = trainWide$FC_LocalSimple_mean3_stderr)
 
-mod <- stan(file = "classifiers/stan/base-model.stan",
+mod <- stan(file = "scripts/base-model.stan",
             data = stan_data, iter = 4000, chains = 3, seed = 123,
             control = list(max_treedepth = 15))
 
@@ -384,30 +411,30 @@ priors <- as.data.frame(mod) %>%
 
 options(mc.cores = parallel::detectCores())
 
-stan_data <- list(N = nrow(feature_matrix2),
-                  y = feature_matrix2$group,
-                  DN_HistogramMode_5 = feature_matrix2$DN_HistogramMode_5,
-                  DN_HistogramMode_10 = feature_matrix2$DN_HistogramMode_10,
-                  CO_f1ecac = feature_matrix2$CO_f1ecac,
-                  CO_FirstMin_ac = feature_matrix2$CO_FirstMin_ac,
-                  CO_HistogramAMI_even_2_5 = feature_matrix2$CO_HistogramAMI_even_2_5,
-                  CO_trev_1_num = feature_matrix2$CO_trev_1_num,
-                  MD_hrv_classic_pnn40 = feature_matrix2$MD_hrv_classic_pnn40,
-                  SB_BinaryStats_mean_longstretch1 = feature_matrix2$SB_BinaryStats_mean_longstretch1,
-                  SB_TransitionMatrix_3ac_sumdiagcov = feature_matrix2$SB_TransitionMatrix_3ac_sumdiagcov,
-                  PD_PeriodicityWang_th0_01 = feature_matrix2$PD_PeriodicityWang_th0_01,
-                  CO_Embed2_Dist_tau_d_expfit_meandiff = feature_matrix2$CO_Embed2_Dist_tau_d_expfit_meandiff,
-                  IN_AutoMutualInfoStats_40_gaussian_fmmi = feature_matrix2$IN_AutoMutualInfoStats_40_gaussian_fmmi,
-                  FC_LocalSimple_mean1_tauresrat = feature_matrix2$FC_LocalSimple_mean1_tauresrat,
-                  DN_OutlierInclude_p_001_mdrmd = feature_matrix2$DN_OutlierInclude_p_001_mdrmd,
-                  DN_OutlierInclude_n_001_mdrmd = feature_matrix2$DN_OutlierInclude_n_001_mdrmd,
-                  SP_Summaries_welch_rect_area_5_1 = feature_matrix2$SP_Summaries_welch_rect_area_5_1,
-                  SB_BinaryStats_diff_longstretch0 = feature_matrix2$SB_BinaryStats_diff_longstretch0,
-                  SB_MotifThree_quantile_hh = feature_matrix2$SB_MotifThree_quantile_hh,
-                  SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1 = feature_matrix2$SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1,
-                  SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1 = feature_matrix2$SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1,
-                  SP_Summaries_welch_rect_centroid = feature_matrix2$SP_Summaries_welch_rect_centroid,
-                  FC_LocalSimple_mean3_stderr = feature_matrix2$FC_LocalSimple_mean3_stderr,
+stan_data <- list(N = nrow(testWide),
+                  y = testWide$group,
+                  DN_HistogramMode_5 = testWide$DN_HistogramMode_5,
+                  DN_HistogramMode_10 = testWide$DN_HistogramMode_10,
+                  CO_f1ecac = testWide$CO_f1ecac,
+                  CO_FirstMin_ac = testWide$CO_FirstMin_ac,
+                  CO_HistogramAMI_even_2_5 = testWide$CO_HistogramAMI_even_2_5,
+                  CO_trev_1_num = testWide$CO_trev_1_num,
+                  MD_hrv_classic_pnn40 = testWide$MD_hrv_classic_pnn40,
+                  SB_BinaryStats_mean_longstretch1 = testWide$SB_BinaryStats_mean_longstretch1,
+                  SB_TransitionMatrix_3ac_sumdiagcov = testWide$SB_TransitionMatrix_3ac_sumdiagcov,
+                  PD_PeriodicityWang_th0_01 = testWide$PD_PeriodicityWang_th0_01,
+                  CO_Embed2_Dist_tau_d_expfit_meandiff = testWide$CO_Embed2_Dist_tau_d_expfit_meandiff,
+                  IN_AutoMutualInfoStats_40_gaussian_fmmi = testWide$IN_AutoMutualInfoStats_40_gaussian_fmmi,
+                  FC_LocalSimple_mean1_tauresrat = testWide$FC_LocalSimple_mean1_tauresrat,
+                  DN_OutlierInclude_p_001_mdrmd = testWide$DN_OutlierInclude_p_001_mdrmd,
+                  DN_OutlierInclude_n_001_mdrmd = testWide$DN_OutlierInclude_n_001_mdrmd,
+                  SP_Summaries_welch_rect_area_5_1 = testWide$SP_Summaries_welch_rect_area_5_1,
+                  SB_BinaryStats_diff_longstretch0 = testWide$SB_BinaryStats_diff_longstretch0,
+                  SB_MotifThree_quantile_hh = testWide$SB_MotifThree_quantile_hh,
+                  SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1 = testWide$SC_FluctAnal_2_rsrangefit_50_1_logi_prop_r1,
+                  SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1 = testWide$SC_FluctAnal_2_dfa_50_1_2_logi_prop_r1,
+                  SP_Summaries_welch_rect_centroid = testWide$SP_Summaries_welch_rect_centroid,
+                  FC_LocalSimple_mean3_stderr = testWide$FC_LocalSimple_mean3_stderr,
                   alpha_mean = priors$alpha_mean,
                   alpha_sd = priors$alpha_sd,
                   DN_HistogramMode_5_mean = priors$beta_1_mean,
@@ -455,13 +482,9 @@ stan_data <- list(N = nrow(feature_matrix2),
                   FC_LocalSimple_mean3_stderr_mean = priors$beta_22_mean,
                   FC_LocalSimple_mean3_stderr_sd = priors$beta_22_sd)
 
-# Prep memory limits
-
-rm(feature_matrix_cobre, feature_matrix1, feature_matrix2, priors)
-
 # Fit model
 
-mod2 <- stan(file = "classifiers/stan/informed-model.stan",
+mod2 <- stan(file = "scripts/informed-model.stan",
              data = stan_data, iter = 4000, chains = 3, seed = 123,
              control = list(max_treedepth = 15))
 
@@ -475,8 +498,80 @@ as.data.frame(mod2) %>%
   geom_histogram(aes(y = after_stat(ndensity)), binwidth = 0.1, fill = "#E494D3", alpha = 0.8) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   labs(title = "Posterior distributions for SonyAIBORobotSurface1 data with informative priors",
-       subtitle = "Bayesian logistic regression model run in Stan with 3 chains and 4,000 iterations per chain. Histogram binwidth = 0.1 and scaled to max of 1",
+       subtitle = "Bayesian logistic regression model run in Stan with 3 chains and 4,000 iterations per chain.",
        x = "Coefficient Value",
-       y = "Density") +
-  theme_bw() +
+       y = "Density",
+       caption = "Histogram binwidth = 0.1 and density scaled to max of 1.") +
   facet_wrap(~parameter)
+
+#----------------------------------
+# Plot posteriors against prior and 
+# likelihood
+#----------------------------------
+
+# Uninformed priors
+
+features <- colnames(as.data.frame(mod2))[2:23]
+features <- gsub("beta_","\\1",features)
+storage <- list()
+
+for(f in features){
+  mypriors <- data.frame(x = seq(from = -10, to = 10, length.out = nrow(as.data.frame(mod2)))) %>%
+    mutate(value = LaplacesDemon::dst(x, mu = 0, sigma = 2.5, nu = 7),
+           category = "Uninformed Prior") %>%
+    dplyr::select(-c(x)) %>%
+    mutate(parameter = f)
+  
+  storage[[f]] <- mypriors
+}
+
+data2Prior <- rbindlist(storage, use.names = TRUE)
+
+# Informed priors
+
+storage2 <- list()
+nums <- seq(from = 3, to = ncol(priors)-1, by = 2)
+
+for(f in nums){
+  
+  mypriors2 <- data.frame(x = seq(from = -10, to = 10, length.out = nrow(as.data.frame(mod2)))) %>%
+    mutate(value = dnorm(x, mean = priors[,f], sd = priors[,f+1]),
+           category = "Informed Prior") %>%
+    mutate(parameter = f)
+  
+  storage2[[f]] <- mypriors2
+}
+
+indexToFeature <- data.frame(parameter = seq(from = 3, to = ncol(priors)-1, by = 2),
+                             parameterProper = features)
+
+data2InfPrior <- rbindlist(storage2, use.names = TRUE) %>%
+  left_join(indexToFeature, by = c("parameter" = "parameter")) %>%
+  dplyr::select(-c(parameter)) %>%
+  rename(parameter = parameterProper)
+
+# Plot
+
+palette2 <- c("Uninformed Prior"= "#E494D3", 
+              "Informed Prior"= "#87DCC0", 
+              "Posterior Estimates" = "#88BBE4")
+
+as.data.frame(mod2) %>%
+  dplyr::select(c(2:23)) %>%
+  gather(key = parameter, value = value, 1:22) %>%
+  mutate(category = "Posterior Estimates",
+         parameter = gsub("beta_", "\\1", parameter)) %>%
+  bind_rows(data2Prior, data2InfPrior) %>%
+  mutate(category = factor(category, levels = c("Uninformed Prior", "Informed Prior", "Posterior Estimates"))) %>%
+  ggplot(aes(x = value, fill = category)) +
+  geom_histogram(aes(y = after_stat(ndensity)), binwidth = 0.2, alpha = 0.5) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(title = "Posterior distributions for SonyAIBORobotSurface1 data with informative priors",
+       subtitle = "Bayesian logistic regression model run in Stan with 3 chains and 4,000 iterations per chain.",
+       x = "Coefficient Value",
+       y = "Density",
+       fill = NULL) +
+  scale_colour_manual(values = palette2) +
+  theme(legend.position = "bottom",
+        legend.key = element_blank()) +
+  facet_wrap(~parameter, scales = "free")
